3e1f34a69251f858701f632365cbac86e35b04db
Karst Hammer <k.hammer@youngcapital.nl>
2017-07-13 17:30:44 +0200

    Now it works

--- lib/sidekiq/killer.rb
+++ lib/sidekiq/killer.rb
@@ -2,6 +2,6 @@ require "sidekiq/killer/version"
 
 module Sidekiq
   module Killer
-    require 'sidekiq/killer/memory'
+    require "sidekiq/killer/memory"
   end
 end
--- lib/sidekiq/killer/memory.rb
+++ lib/sidekiq/killer/memory.rb
@@ -1,11 +1,15 @@
+require 'get_process_mem'
+
 module Sidekiq
   module Killer
     class Memory
       def initialize(options = {})
-        @max_rss         = (options['max_rss']         || 0).to_s.to_i
-        @grace_time      = (options['grace_time']      || 0).to_s.to_i
-        @shutdown_wait   = (options['shutdown_wait']   || 15 * 60).to_s.to_i
-        @shutdown_signal = (options['shutdown_signal'] || 'SIGKILL').to_s
+        options.stringify_keys!
+
+        @max_rss         = (options["max_rss"]         || 0).to_s.to_i
+        @grace_time      = (options["grace_time"]      || 15 * 60).to_s.to_i
+        @shutdown_wait   = (options["shutdown_wait"]   || 30).to_s.to_i
+        @shutdown_signal = (options["shutdown_signal"] || "SIGKILL").to_s
       end
 
       # Create a mutex used to ensure there will be only one thread waiting to
@@ -22,21 +26,21 @@ module Sidekiq
           # Return if another thread is already waiting to shut Sidekiq down
           return unless MUTEX.try_lock
 
-          Sidekiq.logger.warn 'current RSS #{current_rss} exceeds maximum RSS '\
-            '#{@max_rss}'
-          Sidekiq.logger.warn 'this thread will shut down PID #{Process.pid} - Worker #{worker.class} - JID-#{job['jid']}'\
-            'in #{@grace_time} seconds'
+          Sidekiq.logger.warn "current RSS #{current_rss} exceeds maximum RSS "\
+            "#{@max_rss}"
+          Sidekiq.logger.warn "this thread will shut down PID #{::Process.pid} - Worker #{worker.class} - JID-#{job['jid']}"\
+            "in #{@grace_time} seconds"
           sleep(@grace_time)
 
-          Sidekiq.logger.warn 'sending SIGTERM to PID #{Process.pid} - Worker #{worker.class} - JID-#{job['jid']}'
-          Process.kill('SIGTERM', Process.pid)
+          Sidekiq.logger.warn "sending SIGTERM to PID #{::Process.pid} - Worker #{worker.class} - JID-#{job['jid']}"
+          ::Process.kill("SIGTERM", ::Process.pid)
 
-          Sidekiq.logger.warn 'waiting #{@shutdown_wait} seconds before sending '\
-            '#{@shutdown_signal} to PID #{Process.pid} - Worker #{worker.class} - JID-#{job['jid']}'
+          Sidekiq.logger.warn "waiting #{@shutdown_wait} seconds before sending "\
+            "#{@shutdown_signal} to PID #{::Process.pid} - Worker #{worker.class} - JID-#{job['jid']}"
           sleep(@shutdown_wait)
 
-          Sidekiq.logger.warn 'sending #{@shutdown_signal} to PID #{Process.pid} - Worker #{worker.class} - JID-#{job['jid']}'
-          Process.kill(@shutdown_signal, Process.pid)
+          Sidekiq.logger.warn "sending #{@shutdown_signal} to PID #{::Process.pid} - Worker #{worker.class} - JID-#{job['jid']}"
+          ::Process.kill(@shutdown_signal, ::Process.pid)
         end
       end
 
